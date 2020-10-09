a4dee18c9d1fd3cc865e132590da6875a63b5a2a
logicfirst <slee@logicfirstllc.com>
2020-10-09 14:09:00 -0400

    after lunch

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,10 @@
+require_relative './owner.rb'
+
 class Cat
-  # code goes here
+  
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+  end
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,34 @@
+require 'pry'
+
 class Owner
-  # code goes here
+
+  @@all = []
+  attr_reader :name, :species, :say_species
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+
+  def say_species
+    return "I am a human."
+  end
+
+  def self.all
+    return @@all
+  end
+
+  def self.count
+    return @@all.count
+  end
+
+  def self.reset_all
+    # return @@all.clear()
+    return @@all = []
+  end
+    
+
 end
+
+puts "hello"
